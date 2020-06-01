513cec9402c0bf4589dc553a1543a3ccc495e915
Mrcavas <59727464+Mrcavas@users.noreply.github.com>
2020-06-01 15:27:36 +0200

    Update and rename index.py to index.cgi

---
+++ index.cgi
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+print("Content-Type: text/html\r\n")
+print("hello world")
--- index.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-# //coding: utf-8
-# coding=utf-8
-# vim: set fileencoding=utf-8 :
-print("Content-Type: text/html\r\n")
-print("hello world")
